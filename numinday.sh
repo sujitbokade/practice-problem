6e7166a06fd7a5530eb1b1c67602c63aff629613
sujit <sujitbokade2014@gmail.com>
2022-06-18 21:27:37 +0530

    [sujit]make some changes

--- numinday.sh
+++ numinday.sh
@@ -2,18 +2,18 @@
 echo  "Enter a number: "
 read number
 case $number in
-     1)  echo "SUNDAY"
+     1)  echo  "MONDAY"
      ;;
-     2)  echo  "MONDAY"
+     2)  echo  "TUESDAY"
      ;;
-     3)  echo  "TUESDAY"
+     3)   echo "WEDNESDAY"
      ;;
-     4)   echo "WEDNESDAY"
+     4)   echo "THURSDAY"
      ;;
-     5)   echo "THURSDAY"
+     5)   echo "FRIDAY"
      ;;
-     6)   echo "FRIDAY"
+     6)   echo "SATURDAY"
      ;;
-     *)
-      echo "SATURDAY"
+     7)
+          echo "SUNDAY"
 esac
